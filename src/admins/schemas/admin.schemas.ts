dc3f153cec010d46358ef0cd044fc7c72c64cdea
faturahman <faturmriffa@gmail.com>
2026-01-22 10:20:25 +0700

    fix : shcemas delete emaiil prop

--- src/admins/schemas/admin.schemas.ts
+++ src/admins/schemas/admin.schemas.ts
@@ -8,18 +8,13 @@ export class Admin {
     @Prop({ required: true, unique: true })
     name: string;
 
-    // Ubah jadi required: false dulu biar ga error kalau data lama ga punya email
-    @Prop({ required: false }) 
-    email: string;
-
     @Prop({ required: false, default: null })
     photo: string;
 
-    // PENTING: Ganti 'role' jadi 'level' supaya cocok sama Database & Auth Service
     @Prop({ 
         required: true, 
         default: "staff", 
-        enum: ["superadmin", "editor", "staff"] // Sesuaikan enum dengan kebutuhan
+        enum: ["superadmin", "editor", "staff"] 
     })
     level: string; 
 
